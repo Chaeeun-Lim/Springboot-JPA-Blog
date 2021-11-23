f4729a23679de4c1cbb7ac85dfaf4f630a099534
LCE <0315my@naver.com>
2021-11-23 22:11:33 +0900

    환경세팅 완료

---
+++ src/test/java/com/lce/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lce.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
